8185ee40b9419c961a6a46a34c36f44efd17367a
Mohammad Ismail Chaudhary <125667352+ismaehl-2002@users.noreply.github.com>
2023-02-18 14:51:50 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Enantiomers</p>"
 
-#localjost chhaiye 
+@app.route("/")
+def hello_enantiomer():
+  return render_template('home.html')
+
+
+#localjost chhaiye
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
